24115ddd2990ab42b72bab586bbc7af02e1f71bc
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2016-10-27 13:39:20 +0700

    Angular2 Tour of Heroes Tutorial: Part2 - The Hero Editor

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ export class Hero {
 
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+    templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
     title = 'Tour of Heroes';
