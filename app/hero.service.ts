e99579092e28d9ad23fb31904af65d2ee73bfa40
Muhammad Hariz Faza <m.hariz.faza@gmail.com>
2016-10-27 14:59:14 +0700

    Angular2 Tour of Heroes Tutorial: Part5 - Services(Add Delay 2 Seconds Method)

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -11,4 +11,10 @@ export class HeroService {
         return Promise.resolve(HEROES);
     }
 
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise<Hero[]>(resolve =>
+                                  setTimeout(resolve, 2000))
+                                  .then(() => this.getHeroes());
+    }
+
 }
